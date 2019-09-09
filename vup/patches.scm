8b5640bc704387c22e1dc9bcd3519783ff126069
Robin Ole Heinemann <robin.ole.heinemann@t-online.de>
2019-09-09 22:05:12 +0200

    add xkeyboard-config patch

---
+++ vup/patches.scm
@@ -0,0 +1,14 @@
+(define-module (vup patches)
+  #:use-module (guix packages)
+  #:use-module (gnu packages xorg))
+
+;; patch xkeyboard-config to include my vup layout
+(let* 
+  ((old-xkeyboard-config (module-ref (resolve-module '(gnu packages xorg)) 'xkeyboard-config))
+   (old-source (package-source old-xkeyboard-config)))
+
+  (module-define! (resolve-module '(gnu packages xorg)) 'xkeyboard-config
+    (package (inherit old-xkeyboard-config)
+      (source (origin 
+                (inherit old-source)
+                (patches (append (origin-patches old-source) '("vup.patch"))))))))
