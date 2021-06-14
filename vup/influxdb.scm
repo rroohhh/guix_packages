e736299ba07d5ab439102a599348c278d71a752a
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2021-06-14 18:06:28 +0200

    fix telegraf (it is actually dynamically linked)

--- vup/influxdb.scm
+++ vup/influxdb.scm
@@ -5,6 +5,9 @@
   #:use-module (guix utils)
   #:use-module (guix build-system copy)
   #:use-module (gnu packages)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages bootstrap)
+  #:use-module (gnu packages elf)
   #:use-module ((guix licenses) #:prefix license:))
 
 (define-public influxd
@@ -48,12 +51,24 @@
        (sha256
         (base32 "10vf1jh9ay5zxfbpn5c78h6g7bf1356hn5d2xd824pcbnjz19pmk"))))
     (build-system copy-build-system)
+    (inputs `(("libc" ,glibc)
+              ("patchelf" ,patchelf)))
     (arguments
-     '(#:install-plan
-       '(("usr/bin/telegraf" "bin/telegraf"))))
+     `(#:install-plan
+       '(("usr/bin/telegraf" "bin/telegraf"))
+       #:phases (modify-phases %standard-phases
+                  (add-after 'install 'patch
+                    (lambda* (#:key inputs outputs #:allow-other-keys)
+                      (let* ((ld-so (string-append (assoc-ref inputs "libc")
+                                                   ,(glibc-dynamic-linker)))
+                             (out (assoc-ref outputs "out"))
+                             (bin (string-append out "/bin/telegraf")))
+                        (invoke "patchelf" "--set-interpreter" ld-so bin)))))))
     (home-page
       "https://www.influxdata.com/")
     (synopsis "Telegraf is a plugin-driven server agent for collecting and sending metrics and events from databases, systems, and IoT sensors.")
     (description
       "Telegraf is a plugin-driven server agent for collecting and sending metrics and events from databases, systems, and IoT sensors.")
     (license #f)))                      ; MIT
+
+telegraf
