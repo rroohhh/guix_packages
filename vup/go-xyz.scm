7e5a169e5e38ffb62d622131c5e93a565ae39e1e
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2021-07-19 22:54:06 +0200

    add rtsp-simple-server

--- vup/go-xyz.scm
+++ vup/go-xyz.scm
@@ -2,6 +2,7 @@
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix build-system trivial)
+  #:use-module (guix build-system copy)
   #:use-module (gnu packages)
   #:use-module (gnu packages base)
   #:use-module (gnu packages compression)
@@ -34,3 +35,25 @@
     (description
       "A tool for managing secrets")
     (license license:mpl2.0)))
+
+(define-public rtsp-simple-server
+  (package
+    (name "rtsp-simple-server")
+    (version "0.16.4")
+    (source
+     (origin
+       (method url-fetch/tarbomb)
+       (uri (string-append "https://github.com/aler9/rtsp-simple-server/releases/download/v" version "/rtsp-simple-server_v" version "_linux_amd64.tar.gz"""))
+       (sha256
+        (base32 "1nabx2zmkx758cw7szg118x61hl2y8hp13a5pai4f3q391vag7h6"))))
+    (build-system copy-build-system)
+    (arguments
+     '(#:install-plan
+       '(("rtsp-simple-server" "bin/rtsp-simple-server")
+         ("rtsp-simple-server.yml" "etc/rtsp-simple-server"))))
+    (home-page
+      "https://github.com/aler9/rtsp-simple-server")
+    (synopsis "ready-to-use RTSP / RTMP / HLS server and proxy that allows to read, publish and proxy video and audio streams")
+    (description
+      "ready-to-use RTSP / RTMP / HLS server and proxy that allows to read, publish and proxy video and audio streams")
+    (license #f))) ;; mit
