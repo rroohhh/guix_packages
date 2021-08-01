6f1ee6ce38e41afd8a418c9a8ef6cf83d11b8082
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2021-08-01 20:25:42 +0200

    add klayout

--- vup/qt-apps.scm
+++ vup/qt-apps.scm
@@ -4,6 +4,7 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system qt)
   #:use-module (gnu packages qt)
+  #:use-module (gnu packages python)
   #:use-module (gnu packages compression))
 
 (define-public qdirstat
@@ -34,3 +35,61 @@
    (synopsis "QDirStat - Qt-based directory statistics (KDirStat without any KDE - from the original KDirStat author)")
    (description "QDirStat - Qt-based directory statistics (KDirStat without any KDE - from the original KDirStat author)")
    (license license:gpl2)))
+
+
+(define-public klayout
+  (package
+   (name "klayout")
+   (version "0.27.3")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/KLayout/klayout")
+           (commit "v0.27.3")))
+     (file-name (git-file-name name version))
+     (sha256
+      (base32 "0kd0sspyhs29ql7a7zs6ynh5qi0hz892gy0iqqgibbks3nhx03za"))))
+   (arguments
+    `(#:phases
+      (modify-phases %standard-phases
+        (delete 'check)
+        (replace 'configure
+          (lambda* (#:key outputs #:allow-other-keys)
+            (mkdir "build")
+            (chdir "build")
+            (invoke "qmake"
+                    (string-append "PREFIX=" (assoc-ref outputs "out"))
+                    "HAVE_QT5=1"
+                    "HAVE_QT=0"
+                    "HAVE_QT_NETWORK=1"
+                    "HAVE_QT_UITOOLS=1"
+                    "HAVE_QT_SVG=1"
+                    "HAVE_QT_SQL=1"
+                    "HAVE_QT_PRINTSUPPORT=1"
+                    "HAVE_QT_XML=1"
+                    "HAVE_QT_DESIGNER=1"
+                    "HAVE_QT_MULTIMEDIA=1"
+                    "HAVE_QTBINDINGS=1"
+                    "HAVE_QT=1"
+                    "HAVE_PYTHON=0"
+                    "HAVE_RUBY=0"
+                    "HAVE_64BIT_COORD=1"
+                    "CONFIG+=release"
+                    "-recursive"
+                    "../src/klayout.pro")
+            #t)))))
+   (build-system qt-build-system)
+
+   (inputs
+    `(("qtbase" ,qtbase-5)
+      ("qtsvg" ,qtsvg)
+      ("qtxmlpatterns" ,qtxmlpatterns)
+      ("qttools" ,qttools)
+      ("qtmultimedia" ,qtmultimedia)
+      ("python" ,python)
+      ("zlib" ,zlib)))
+   (home-page "https://github.com/KLayout/klayout")
+   (synopsis "KLayout")
+   (description "KLayout")
+   (license #f)))
