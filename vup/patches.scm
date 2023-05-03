f1358696a90145c03e2bd6fa77d02ae0adebfae5
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2023-05-03 17:47:10 +0200

    fix patch rev and hash

--- vup/patches.scm
+++ vup/patches.scm
@@ -10,7 +10,7 @@
    (old-source (package-source old-xkeyboard-config))
    (patched-pkg (package (inherit old-xkeyboard-config)
 	          (source (origin (inherit old-source)
-				  (patches (append (origin-patches old-source) (list (origin (method url-fetch) (uri "https://raw.githubusercontent.com/rroohhh/guix_packages/825ffca/vup.patch") (sha256 "0ayvi8js754vwfq2zlf5bmv3fq0f5phdl5vrh0b8fws0vmni977w"))))))))))
+				  (patches (append (origin-patches old-source) (list (origin (method url-fetch) (uri "https://raw.githubusercontent.com/rroohhh/guix_packages/1e057f94dd/vup.patch") (sha256 "1n5r518qgfjylqc463rcv9a4dz4ar4m723h21mk0dh50l9fa0k8d"))))))))))
 
   (if (not (package-replacement old-xkeyboard-config))
     (module-define! (resolve-module '(gnu packages xorg)) 'xkeyboard-config
