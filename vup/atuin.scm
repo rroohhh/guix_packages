09ead5c99a62dc505410d12f0598da3d5178694d
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2024-03-05 19:56:48 +0100

    atuin: include crates-web

--- vup/atuin.scm
+++ vup/atuin.scm
@@ -3,6 +3,7 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (gnu packages crates-crypto)
+  #:use-module (gnu packages crates-web)
   #:use-module (gnu packages crates-io)
   #:use-module (gnu packages crates-graphics)
   #:use-module (guix download)
