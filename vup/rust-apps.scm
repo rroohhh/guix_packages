577d529fb343760d70b84a9afd88ece6faafe235
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2025-01-19 21:03:07 +0100

    impl rust-import-lock

--- vup/rust-apps.scm
+++ vup/rust-apps.scm
@@ -1,39 +1,26 @@
 (define-module (vup rust-apps)
+  #:use-module (vup rust-import-lock)
   #:use-module (guix packages)
   #:use-module (guix download)
+  #:use-module (guix git-download)
   #:use-module (guix build-system cargo)
-  #:use-module ((guix import utils) #:select (beautify-description))
-  #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (gnu packages crates-io)
-  #:use-module (gnu packages version-control)
-  #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages xorg)
-  #:use-module (gnu packages python)
-  #:use-module (gnu packages crates-graphics)
-  #:use-module (gnu packages crypto)
-  #:use-module (gnu packages tls))
+  #:use-module ((guix licenses) #:prefix license:))
 
 (define-public ra-multiplex
   (package
     (name "ra-multiplex")
-    (version "0.2.2")
+    (version "0.2.5")
     (source (origin
-              (method url-fetch)
-              (uri (crate-uri "ra-multiplex" version))
-              (file-name (string-append name "-" version ".tar.gz"))
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/pr2502/ra-multiplex")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
               (sha256
                (base32
-                "0ph42x923j9i23y2x59g93ggv885k1ldixs5bg0xkqhmf43nsm3y"))))
+                "0bz22ksgw26m5zzqpjakc8a4z8xpac6sp1z9n30746cc1zvff6k8"))))
     (build-system cargo-build-system)
-    (arguments
-     `(#:cargo-inputs (("rust-anyhow" ,rust-anyhow-1)
-                       ("rust-directories" ,rust-directories-4)
-                       ("rust-env-logger" ,rust-env-logger-0.9)
-                       ("rust-log" ,rust-log-0.4)
-                       ("rust-serde" ,rust-serde-1)
-                       ("rust-serde-json" ,rust-serde-json-1)
-                       ("rust-tokio" ,rust-tokio-1)
-                       ("rust-toml" ,rust-toml-0.5))))
+    (inputs (import-rust-lock source))
     (home-page "https://github.com/pr2502/ra-multiplex")
     (synopsis
      "share one rust-analyzer server instance between multiple LSP clients to save resources")
@@ -41,3 +28,5 @@
      "share one rust-analyzer server instance between multiple LSP clients to save
 resources")
     (license license:expat)))
+
+ra-multiplex
